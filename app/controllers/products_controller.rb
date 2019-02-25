f45831503e6d056d7647098ac88b819ac95125a8
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-25 22:49:47 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,6 +14,10 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
   
+  def show
+    @product = Product.find(params[:id])
+  end
+  
   def inventory
     product = Product.find(params[:id])
       if product.inventory > 0
