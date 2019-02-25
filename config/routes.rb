941acf270a737edf5084e18e61098f551c176e53
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-25 22:53:03 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  resources :products
-  get '/products/:id/inventory', to: 'products#inventory'
-  get '/products/:id/description', to: 'products#description'
+
+  resources :products, only: [:new, :create, :show, :index]
+  get 'products/:id/inventory', to: "products#inventory"
+  get 'products/:id/description', to: "products#description"
 end
