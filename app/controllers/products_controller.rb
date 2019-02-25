5cda5129f0069364301250c3c88dd056fa337be1
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-25 22:37:16 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
   
-  
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
   
   def create
     @product = Product.create(product_params)
