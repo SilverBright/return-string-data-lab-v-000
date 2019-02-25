22c81a8ee9c9feef87b51228b1a74a1cdc60e7cc
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-25 22:15:23 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.create
     @product.save
-    redirect_to product_path
+    redirect_to product_path(@product)
   end
 
 end
