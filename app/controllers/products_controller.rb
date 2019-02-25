be56f166f1dd1dd5012c0fceb4e00102810fbe6d
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-25 22:31:24 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,12 +2,11 @@ class ProductsController < ApplicationController
   
   def create
     @product = Product.create(product_params)
-    @product.save
     redirect_to products_path
   end
 
-  # def product_params
-    # params.require(:product).permit(:name, :inventory, :description)
-  # end
+  def product_params
+    params.require(:product).permit(:name, :inventory, :description)
+  end
 
 end
